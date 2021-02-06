05375bbb667e49513d010c6394a4a92ff139b53d
timurgareev <gareevtimura@gmail.com>
2021-02-06 18:34:26 +0500

    модифицировал вызов модального окна

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -97,15 +97,16 @@ window.addEventListener("DOMContentLoaded", function () {
         modal= document.querySelector('.modal'),
         modalCloseBtn = document.querySelector('[data-close]');
 
+    function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove("hide");
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    }
+
     modalTrigger.forEach(btn => {
-        btn.addEventListener('click',()=>{
-            modal.classList.add('show');
-            modal.classList.remove("hide");
-            document.body.style.overflow = 'hidden';
-        });
+        btn.addEventListener('click',openModal); 
     });
-    
-    
 
     function closeModal() {
         modal.classList.add('hide');
@@ -127,5 +128,15 @@ window.addEventListener("DOMContentLoaded", function () {
         }
     });
 
+    const modalTimerId = setTimeout(openModal, 5000);
+
+    function showModalByScroll () {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener("scroll", showModalByScroll);
+        }
+    }
+
+    window.addEventListener("scroll", showModalByScroll);
 
 });
