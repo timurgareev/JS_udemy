0089c770e799d8e4958dfa53fe588b0ba676c674
timurgareev <gareevtimura@gmail.com>
2021-02-06 18:10:25 +0500

    сделал модальные окна

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -93,29 +93,39 @@ window.addEventListener("DOMContentLoaded", function () {
     setClock('.timer', deadline);
 
     // modal 
-    const modalTrigger = document.querySelector('[data-modal]'),
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
         modal= document.querySelector('.modal'),
         modalCloseBtn = document.querySelector('[data-close]');
 
-    modalTrigger.addEventListener('click',()=>{
-        modal.classList.add('show');
-        modal.classList.remove("hide");
-        document.body.style.overflow = 'hidden';
- 
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click',()=>{
+            modal.classList.add('show');
+            modal.classList.remove("hide");
+            document.body.style.overflow = 'hidden';
+        });
     });
+    
+    
 
-    modalCloseBtn.addEventListener('click',()=>{
+    function closeModal() {
         modal.classList.add('hide');
         modal.classList.remove("show");
-        document.body.style.overflow = 'show';
-    }) ;
+        document.body.style.overflow = '';
+    }
 
-    mpdal.addEventListener('click', (e)=>{
+    modalCloseBtn.addEventListener('click',closeModal);
+
+    modal.addEventListener('click', (e)=>{
         if (e.target ===modal) {
-            modal.classList.add('hide');
-            modal.classList.remove("show");
-            document.body.style.overflow = 'show';
+            closeModal();
         }
     });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
+
 
 });
