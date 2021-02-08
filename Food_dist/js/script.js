c8173ebcf3390c1a9223b4a1ca44f66910a9b6e7
timurgareev <gareevtimura@gmail.com>
2021-02-08 21:37:41 +0500

    добавил оператор rest в класс

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -142,12 +142,13 @@ window.addEventListener("DOMContentLoaded", function () {
     //используем классы для карточек
 
     class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector){
+        constructor(src, alt, title, descr, price, parentSelector, ...classes){
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
+            this.classes = classes;
             this.parent = document.querySelector(parentSelector);
             this.transfer = 27;
             this.changeToUAH();
@@ -159,8 +160,14 @@ window.addEventListener("DOMContentLoaded", function () {
 
         render(){
             const element = document.createElement('div');
+            if (this.classes.length === 0) {
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+            
             element.innerHTML = `
-                <div class="menu__item">
                     <img src=${this.src} alt=${this.alt}>
                     <h3 class="menu__item-subtitle">${this.title}"</h3>
                     <div class="menu__item-descr">${this.descr}</div>
@@ -169,7 +176,6 @@ window.addEventListener("DOMContentLoaded", function () {
                         <div class="menu__item-cost">Цена:</div>
                         <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                     </div>
-                </div>
             `;
             this.parent.append(element);
         }
@@ -181,7 +187,8 @@ window.addEventListener("DOMContentLoaded", function () {
         'Меню "Фитнес"',
         'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
         9,
-        '.menu .container',
+        '.menu .container'
+        // 'menu__item',
     ).render();
 
     new MenuCard(
@@ -191,6 +198,7 @@ window.addEventListener("DOMContentLoaded", function () {
         'меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
         20,
         '.menu .container',
+        'menu__item',
     ).render();
 
     new MenuCard(
@@ -200,6 +208,7 @@ window.addEventListener("DOMContentLoaded", function () {
         'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
         16,
         '.menu .container',
+        'menu__item',
     ).render();     
 
 });
